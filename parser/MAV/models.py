9922259d67fc4d5205c4c67c228632bb2fe85b73
OnionKiller <tomi@csilling.com>
2022-11-09 17:20:01 +0100

    add ORM mapped classes

---
+++ parser/MAV/models.py
@@ -0,0 +1,34 @@
+from uuid import uuid4
+from sqlalchemy.orm import declarative_base #type:ignore
+from sqlalchemy_utils import UUIDType,JSONType #type:ignore
+from sqlalchemy import Integer,Date,String #type:ignore
+from sqlalchemy import Column, ForeignKey #type:ignore
+
+UUID = UUIDType(native=True)
+
+DeclarativeBase = declarative_base()
+
+class MAVEvent(DeclarativeBase):
+    '''Simple class to hold event instances.
+    The events are handled as immutable objects regarding the RSS feed.
+    The (main) properties of an event are:
+    ---
+
+    `id`: Used by the RSS feed, this indicates corresponding events
+    `last_modification`: Event timestamp when the event was last modified
+    `update_uuid`: UUID of the next update of the event. Events may have multiple updated, then
+        the event instaces are chained with this.
+    '''
+    __tablename__ = "RSS_events_MAV"
+    
+    uuid = Column(UUID, primary_key=True,nullable=False, unique=True, default=uuid4)
+    id = Column(Integer,nullable=False)
+    last_modification = Column(Date,nullable=False)
+    update_uuid=Column(UUID)
+    title = Column(String(50))
+
+class MAVEventDump(DeclarativeBase):
+    __tablename__ = "RAW_events_MAV"
+    id = Column(Integer,primary_key=True)
+    event_uuid = Column(UUID, ForeignKey("RSS_events_MAV.uuid",ondelete="SET NULL"))
+    event_dict = Column(JSONType)
