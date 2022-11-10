92a1eca06e459a58116fe60dcb399df4b3f26430
OnionKiller <tomi@csilling.com>
2022-11-10 22:43:03 +0100

    implement basic config management based on dotenv

---
+++ config.py
@@ -0,0 +1,15 @@
+import os
+
+from dotenv import load_dotenv
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+class Config(object):
+    BASE_URL = os.environ.get('BASE_URL', 'http://localhost.')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    def __init__():
+        load_dotenv()
+
