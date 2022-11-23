a3f837f48755e2fb67a303f8d5dff2bf7a55c1e1
OnionKiller <tomi@csilling.com>
2022-11-23 21:56:45 +0100

    fix: catch rate limit return value correctly

--- src/feed_handler/feed_consumer.py
+++ src/feed_handler/feed_consumer.py
@@ -12,6 +12,9 @@ class FeedConsumer:
 
     def fetch(self):
         new_feed_state = feedparser.parse(r'https://www.mavcsoport.hu/mavinform/rss.xml',etag=self._etag)
+        if new_feed_state.status == 304:
+            #TODO log too frequent refresh
+            return self._last_known
         if new_feed_state.status != 200:
             raise NotImplementedError("Handle if feed is down!")
         self._last_known = new_feed_state
