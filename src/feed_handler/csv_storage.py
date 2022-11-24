f5e411a8686f701f72577547a706317bf8297019
OnionKiller <tomi@csilling.com>
2022-11-24 02:59:47 +0100

    fix: time and timestamp export import inparities fix: return type conversion

--- src/feed_handler/csv_storage.py
+++ src/feed_handler/csv_storage.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 from pathlib import Path
-from time import mktime
+from time import mktime, strptime
 from typing import List
 from .reflexive_feed_storage import ReflexiveFeedStorage
 from ..RSS_abstracts.RSSEntry import RSSEntry
@@ -20,13 +20,29 @@ class csvStorage(ReflexiveFeedStorage):
             raise ValueError(f"Path {file_location} is not a csv file.")
 
         if file_location.exists():
-            self._storage = pd.read_csv(file_location,index_col=False)
+            df = pd.read_csv(file_location,index_col=False)
+            #map datetime string to actual datetime
+            df['published_datetime'] = df['published_datetime'].transform(
+                lambda x: datetime.fromtimestamp(mktime(
+                    strptime(x,'%Y-%m-%d %H:%M:%S')    
+                ))
+            )
+            #map str representation of time struct to actual time struct
+            df['published_parsed'] = df['published_datetime'].transform(
+                lambda x: x.timetuple()
+                )
+            self._storage = df
+
         else:
             self._storage = pd.DataFrame()
         
         self._csv_path = file_location
 
         super().__init__()
+
+    def _convert(_,df:pd.DataFrame)->List[RSSEntry]:
+        dicts = df.to_dict(orient='records')
+        return [RSSEntry.from_dict(d) for d in dicts]
 
     def _load_all_event_ids(self) -> List[int]:
         if self._storage.empty:
@@ -37,7 +53,7 @@ class csvStorage(ReflexiveFeedStorage):
     def _load_all_event_w_updates(self, id: int) -> List[RSSEntry]:
         if self._storage.empty:
             return []
-        return self._storage[self._storage.id == id]
+        return self._convert(self._storage[self._storage.id == id])
         
 
     def _store_event(self, Event: RSSEntry):
@@ -55,4 +71,7 @@ class csvStorage(ReflexiveFeedStorage):
 
 
     def _load_latest_event_by_id(self, id) -> RSSEntry | None:
-       return self._load_all_event_w_updates(id).iloc[0]
+        list = self._load_all_event_w_updates(id)
+        if len(list) == 0:
+            return None
+        return list[0]
