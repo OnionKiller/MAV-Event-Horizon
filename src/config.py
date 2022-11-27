6b522f0e50881901c5857d8cc6e19599142d3f66
Lovas Tamás <lovaas.taamaas@gmail.com>
2022-11-27 19:19:58 +0100

    Config adjustment

--- src/config.py
+++ src/config.py
@@ -9,7 +9,8 @@ class Config(object):
     BASE_URL = os.environ.get("BASE_URL", "http://localhost.")
     COGNITIVE_SERVICE_KEY = os.environ.get("COGNITIVE_SERVICE_KEY",None)
     #TODO remove default value
-    COGNITIVE_SERVICE_ENDPOINT = os.environ.get("COGNITIVE_SERVICE_ENDPOINT","https://bme-mav-nlp.cognitiveservices.azure.com/")
+    COGNITIVE_SERVICE_BASE = os.environ.get("COGNITIVE_SERVICE_BASE", "bme-mav-nlp")
+    COGNITIVE_SERVICE_ENDPOINT = os.environ.get("COGNITIVE_SERVICE_ENDPOINT","https://"+COGNITIVE_SERVICE_BASE+".cognitiveservices.azure.com/")
 
     def __init__(self):
         load_dotenv()
