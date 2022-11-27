0f2fb774c63c72bc76aaad7b04105d96a814409b
Lovas Tamás <lovaas.taamaas@gmail.com>
2022-11-27 17:23:06 +0100

    Use RSSEntry's timestamp as startDate

--- src/nlp/incident_handler.py
+++ src/nlp/incident_handler.py
@@ -1,18 +1,19 @@
 import pandas as pd
 from entryToIncident import *
 
+
 class IncidentHandler:
     def __init__(self):
         self.incidents = pd.DataFrame(columns=['Id', 'Line', 'Locations', 'Cause', 'StartDate','EndDate'])
         self.incidents = self.incidents.set_index('Id')
 
     #Todo: RSSentryt és a textet itt megkapni
-    def handleIncident(self, id, text):
+    def handleIncident(self, entry, text):
         try:
             if id in self.incidents.index:
-                self.updateIncident(id, text)
+                self.updateIncident(entry.id, text)
             else:
-                self.addIncident(id, text)
+                self.addIncident(entry.id, entry.published_datetime, text)
         except Exception as e: 
             print(e)
             print("Error with handling the incident!")
@@ -31,22 +32,8 @@ class IncidentHandler:
             change = True
         return change
     
-    def addIncident(self, id, text):
-        line, locations, cause, startDate, endDate = newEntryPipeline(text)
+    def addIncident(self, id, time, text):
+        line, locations, cause, startDate, endDate = newEntryPipeline(text, time)
         newRow = [line, locations, cause, startDate, endDate]
         self.incidents.loc[id] = newRow
         return
-
-def main():
-    incidentHandler = IncidentHandler()
-    incidentHandler.handleIncident("31", "A Budapest-Szolnok-Békéscsaba-Lőkösháza vonalon december 10-ig pályakarbantartási munkák miatt a vonatok módosított menetrend szerint közlekednek.\
- A Lőkösházáról a Keleti pályaudvarra 6:40-kor induló 759-es számú Békés InterCity helyett Lőkösháza és Békéscsaba között, a délutáni Lőkösháza-Kétegyháza,\
- illetve a Kétegyháza-Lőkösháza viszonylatú személyvonatok (7544, 7546, 7545, 7543, 7553) helyett teljes útvonalon pótlóbuszok közlekednek.\
- A 346-os és 347-es számú Dacia nemzetközi vonat módosított menetrend szerint Biharkeresztesen át - kerülő úton közlekedik a Corvin nemzetközi vonatokkal egyesítve.")
-    incidentHandler.handleIncident("21", "Műszaki ok miatt a Debrecenből Tiszafüredre 22:45-kor induló pótlóbusz a teljes útvonalon közlekedik. Emiatt a november 26-án, szombaton a Tiszafüredről Balmazújvárosra 4:46-kor induló vonat nem közlekedik, helyette pótlóbusz szállítja az utasokat.")
-    incidentHandler.handleIncident("23", "Budapest! Helyszínelnek a hatóságok.")
-    incidentHandler.handleIncident("23", "Track conditions are good.")
-    print(incidentHandler.incidents)
-
-if __name__ == "__main__":
-    main()
