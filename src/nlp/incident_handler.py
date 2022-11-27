94bf96e372a770740f424c149c88cbb2bc6a5944
Lovas Tamás <lovaas.taamaas@gmail.com>
2022-11-27 22:31:47 +0100

    Possible saving solution

--- src/nlp/incident_handler.py
+++ src/nlp/incident_handler.py
@@ -1,12 +1,14 @@
 import pandas as pd
+import os
+from pathlib import Path
 from .entryToIncident import editEntryPipeline, newEntryPipeline
 from ..RSS_abstracts.RSSEntry import RSSEntry
 
 
 class IncidentHandler:
-    def __init__(self):
-        self.incidents = pd.DataFrame(columns=['Id', 'Line', 'Locations', 'Cause', 'StartDate','EndDate'])
-        self.incidents = self.incidents.set_index('Id')
+    def __init__(self, path):
+        self._path = path
+        self.__readData(path)
 
     def handleIncident(self, entry:RSSEntry, text:str):
         try:
@@ -29,10 +31,32 @@ class IncidentHandler:
         if self.incidents.loc[id]['EndDate'] != newEndDate:
             self.incidents.loc[id]['EndDate'] = newEndDate
             change = True
-        return change
+        if change:
+            self.__writeData()
+        return
     
     def addIncident(self, id, time, text):
         line, locations, cause, startDate, endDate = newEntryPipeline(text, time)
         newRow = [line, locations, cause, startDate, endDate]
         self.incidents.loc[id] = newRow
+        self.__writeData()
         return
+
+    def __readData(self, path):
+        if not isinstance(path, Path):
+            file_location = Path(path)
+        else:
+            file_location = path
+
+        if file_location.suffix != ".csv":
+            raise ValueError(f"Path {file_location} is not a csv file.")
+
+        if file_location.exists():
+            df = pd.read_csv(file_location)
+            self.incidents = df
+        else:
+            self.incidents = pd.DataFrame(columns=['Id', 'Line', 'Locations', 'Cause', 'StartDate','EndDate'])
+            self.incidents = self.incidents.set_index('Id')
+    
+    def __writeData(self, path):
+        self.incidents.to_csv(self._path, index=True)
